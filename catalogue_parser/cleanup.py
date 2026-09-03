4f813539f26ebc56e4ddbfb5f7007845628cc655
eleazar675 <evangana48@gmail.com>
2026-09-03 16:47:18 +1000

    cleanup script progress

--- catalogue_parser/cleanup.py
+++ catalogue_parser/cleanup.py
@@ -1,5 +1,15 @@
+import json
+import pandas as pd
 from pathlib import Path
 
+#File paths
 BASE_DIR = Path(__file__).resolve().parent
-JSON_FILE = BASE_DIR / "output" / "catalogue.json"
+INPUT_FILE = BASE_DIR / "output" / "catalogue.json"
+OUTPUT_FILE = BASE_DIR / "output" / "clean_catalogue.json"
 
+#Load Catalogues
+with open(INPUT_FILE, "r", encoding="utf-8") as f:
+  data = json.load(f)
+
+print("Catalogue Cleanup")
+print("=======================")
